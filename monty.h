0898487dc0d7088122a61372b89b3184c3de1c00
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-12-14 18:07:15 -0500

    Group several related structures in _STRUCT_ONE_

--- monty.h
+++ monty.h
@@ -9,3 +9,25 @@
 #include <ctype.h>
 
 #endif /* _MONTY_ */
+
+#ifndef _MONTY_STRUCT_ONE_
+#define _MONTY_STRUCT_ONE_
+
+/**
+ * struct stack_s - doubly linked list representation of a stack
+ * @n: integer
+ * @prev: points to the previous element of the stack
+ * @next: points to the next element of the stack
+ *
+ * Description: doubly linked list node structure
+ * for stack, queue algorithms - LIFO, FIFO.
+ *
+ */
+typedef struct stack_s
+{
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
+} stack_t;
+
+#endif /* _MONTY_STRUCT_ONE */
